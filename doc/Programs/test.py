a3bda894f8ee8f7b266cff6f1a37fac182daf806
mhjensen <morten.hjorth-jensen@fys.uio.no>
2019-04-04 18:22:47 -0400

    changing codes

--- doc/Programs/test.py
+++ doc/Programs/test.py
@@ -41,6 +41,7 @@ Masses = pd.read_fwf(infile, usecols=(2,3,4,6,11),
               header=39,
               index_col=False)
 
+
 # Extrapolated values are indicated by '#' in place of the decimal place, so
 # the Ebinding column won't be numeric. Coerce to float and drop these entries.
 Masses['Ebinding'] = pd.to_numeric(Masses['Ebinding'], errors='coerce')
@@ -48,8 +49,9 @@ Masses = Masses.dropna()
 # Convert from keV to MeV.
 Masses['Ebinding'] /= 1000
 
+
 # Group the DataFrame by nucleon number, A.
-Masses = Masses.groupby('Z')
+Masses = Masses.groupby('A')
 # Find the rows of the grouped DataFrame with the maximum binding energy.
 Masses = Masses.apply(lambda t: t[t.Ebinding==t.Ebinding.max()])
 A = Masses['A']
@@ -58,56 +60,9 @@ N = Masses['N']
 Element = Masses['Element']
 Energies = Masses['Ebinding']
 
-print(Masses)
 
 
-# Now we set up the design matrix X
-X = np.zeros((5,len(A)))
-X[4,:] = A**(-1.0)
-X[3,:] = A**(-1.0/3.0)
-X[2,:] = A**(2.0/3.0)
-X[1,:] = A
-X[0,:] = 1
+df = pd.DataFrame({'A':A,'Z':Z, 'N':N,'Element':Element,'Energies':Energies})
 
-
-clf = skl.LinearRegression().fit(X.T, Energies)
-
-fity = clf.predict(X.T)
-
-# The mean squared error                               
-print("Mean squared error: %.2f" % mean_squared_error(Energies, fity))
-# Explained variance score: 1 is perfect prediction                                 
-print('Variance score: %.2f' % r2_score(Energies, fity))
-# Mean absolute error                                                           
-print('Mean absolute error: %.2f' % mean_absolute_error(Energies, fity))
-print(clf.coef_, clf.intercept_)
-
-
-_lambda = 0.1
-clf_ridge = skl.Ridge(alpha=_lambda).fit(X.T, Energies)
-
-E_ridge = clf_ridge.predict(X.T)
-
-# The mean squared error                               
-print("Mean squared error: %.2f" % mean_squared_error(Energies, E_ridge))
-# Explained variance score: 1 is perfect prediction                                 
-print('Variance score: %.2f' % r2_score(Energies, E_ridge))
-# Mean absolute error                                                           
-print('Mean absolute error: %.2f' % mean_absolute_error(Energies, E_ridge))
-print(clf_ridge.coef_, clf_ridge.intercept_)
-
-
-
-Masses['Eapprox']  = E_ridge
-# Generate a plot comparing the experimental with the fitted values values.
-fig, ax = plt.subplots()
-ax.set_xlabel(r'$A = N + Z$')
-ax.set_ylabel(r'$E_\mathrm{bind}\,/\mathrm{MeV}$')
-ax.plot(Masses['A'], Masses['Ebinding'], alpha=0.7, lw=2,
-            label='Ame2016')
-ax.plot(Masses['A'], Masses['Eapprox'], alpha=0.7, lw=2, c='m',
-            label='Fit')
-ax.legend()
-save_fig("Masses2016")
-plt.show()
+print(df)
 
