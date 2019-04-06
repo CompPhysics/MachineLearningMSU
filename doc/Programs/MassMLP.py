55adce580b5d536003097e7b60b1b88c3e9bb354
mhjensen <morten.hjorth-jensen@fys.uio.no>
2019-04-06 10:46:08 -0400

    added MLP piece

--- doc/Programs/MassMLP.py
+++ doc/Programs/MassMLP.py
@@ -51,7 +51,7 @@ Masses['Ebinding'] /= 1000
 # Group the DataFrame by nucleon number, A.
 Masses = Masses.groupby('A')
 # Find the rows of the grouped DataFrame with the maximum binding energy.
-Masses = Masses.apply(lambda t: t[t.Ebinding==t.Ebinding.max()])
+Masses = Masses.apply(lambda t: t[t.Ebinding==t.Ebinding])
 A = Masses['A']
 Z = Masses['Z']
 N = Masses['N']
@@ -68,44 +68,26 @@ X[0,:] = 1
 
 X_train = X.T
 Y_train = Energies
-n_hidden_neurons = 50
-from sklearn.neural_network import MLPClassifier
+n_hidden_neurons = 100
+epochs = 100
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import accuracy_score
 # store models for later use
 eta_vals = np.logspace(-5, 1, 7)
 lmbd_vals = np.logspace(-5, 1, 7)
 # store the models for later use
 DNN_scikit = np.zeros((len(eta_vals), len(lmbd_vals)), dtype=object)
-
+train_accuracy = np.zeros((len(eta_vals), len(lmbd_vals)))
+import seaborn as sns
+sns.set()
 for i, eta in enumerate(eta_vals):
     for j, lmbd in enumerate(lmbd_vals):
-        dnn = MLPClassifier(hidden_layer_sizes=(n_hidden_neurons), activation='logistic',
+        dnn = MLPRegressor(hidden_layer_sizes=(n_hidden_neurons), activation='logistic',
                             alpha=lmbd, learning_rate_init=eta, max_iter=epochs)
         dnn.fit(X_train, Y_train)
-        
         DNN_scikit[i][j] = dnn
-        
-        print("Learning rate  = ", eta)
-        print("Lambda = ", lmbd)
-        print("Accuracy score on test set: ", dnn.score(X_train, Y_train))
-        print()
+        train_accuracy[i][j] = dnn.score(X_train, Y_train)
 
-
-import seaborn as sns
-
-sns.set()
-
-train_accuracy = np.zeros((len(eta_vals), len(lmbd_vals)))
-test_accuracy = np.zeros((len(eta_vals), len(lmbd_vals)))
-
-for i in range(len(eta_vals)):
-    for j in range(len(lmbd_vals)):
-        dnn = DNN_scikit[i][j]
-        
-        train_pred = dnn.predict(X_train) 
-
-        train_accuracy[i][j] = accuracy_score(Y_train, train_pred)
-
-        
 fig, ax = plt.subplots(figsize = (10, 10))
 sns.heatmap(train_accuracy, annot=True, ax=ax, cmap="viridis")
 ax.set_title("Training Accuracy")
